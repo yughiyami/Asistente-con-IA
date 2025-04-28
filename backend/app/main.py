b2079214a5cc3bcdffa0f91e40f8cbc8c81f05f3
Gabriel Bernedo Kaseng <84144762+Darkfrost-1107@users.noreply.github.com>
2025-04-28 15:36:35 -0500

    feat: aea commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,10 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://192.168.56.1:3000"],
+    allow_origins=[
+        "http://192.168.56.1:3000",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
